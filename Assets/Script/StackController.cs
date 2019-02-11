f4baf60bd9bfc52d5a8323abe6461528f1439282
Daiki Terai <terai@360ch.tv>
2019-02-11 17:42:46 +0900

    undostack, redostackの操作ファイル

--- Assets/Script/StackController.cs
+++ Assets/Script/StackController.cs
@@ -7,19 +7,34 @@ public class StackController : MonoBehaviour
     private Stack<Command> UndoStack = new Stack<Command>();
     private Stack<Command> RedoStack = new Stack<Command>();
 
+    public Command currentCommand { get; private set; }
+
     public void Record(Command command)
     {
         UndoStack.Push(command);
-        Debug.Log("count" + UndoStack.Count);
+        RedoStack.Clear();
+        Debug.Log("Undocount" + UndoStack.Count + "Redocount" + RedoStack.Count);
     }
 
     public void Undo()
     {
-
+        if (UndoStack.Count == 0)
+        {
+            return;
+        }
+        currentCommand = UndoStack.Pop();
+        RedoStack.Push(currentCommand);
+        Debug.Log("Undocount" + UndoStack.Count + "Redocount" + RedoStack.Count);
     }
 
     public void Redo()
     {
-
+        if (RedoStack.Count == 0)
+        {
+            return;
+        }
+        currentCommand = RedoStack.Pop();
+        UndoStack.Push(currentCommand);
+        Debug.Log("Undocount" + UndoStack.Count + "Redocount" + RedoStack.Count);
     }
 }
