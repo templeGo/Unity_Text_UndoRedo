4cbda6eba5d30a13b4dabf7fe214d3ff10fade45
Daiki Terai <terai@360ch.tv>
2019-02-11 18:09:38 +0900

    inputFieldの動きを修正するcustomInputFieldの作成

---
+++ Assets/Script/CustomInputField.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomInputField : InputField
+{
+
+    //TODO inputField選択時に全選択になるのを解除しようと思うも、できず(ActiveInputFieldが呼ばれてない)
+
+    private bool Focused = false;
+
+    new public void ActivateInputField()
+    {
+        Focused = true;
+        base.ActivateInputField();
+    }
+
+    protected override void LateUpdate()
+    {
+        base.LateUpdate();
+        if (Focused)
+        {
+            Debug.Log("Focused"+ Focused);
+            MoveTextEnd(true);
+            Focused = false;
+        }
+    }
+}
