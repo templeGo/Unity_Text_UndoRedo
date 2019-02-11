a847e7c99fd55b4aa23884a11fea60635632877b
Daiki Terai <terai@360ch.tv>
2019-02-11 18:34:29 +0900

    undoスタックに上限をつけた

--- Assets/Script/StackController.cs
+++ Assets/Script/StackController.cs
@@ -13,15 +13,15 @@ public class StackController : MonoBehaviour
     {
         UndoStack.Push(command);
         RedoStack.Clear();
+        CheckStackWeight();
         Debug.Log("Undocount" + UndoStack.Count + "Redocount" + RedoStack.Count);
     }
 
     public void Undo()
     {
         if (UndoStack.Count == 0)
-        {
             return;
-        }
+        
         currentCommand = UndoStack.Pop();
         RedoStack.Push(currentCommand);
         Debug.Log("Undocount" + UndoStack.Count + "Redocount" + RedoStack.Count);
@@ -30,11 +30,44 @@ public class StackController : MonoBehaviour
     public void Redo()
     {
         if (RedoStack.Count == 0)
-        {
             return;
-        }
+        
         currentCommand = RedoStack.Pop();
         UndoStack.Push(currentCommand);
         Debug.Log("Undocount" + UndoStack.Count + "Redocount" + RedoStack.Count);
+    }
+
+    /// <summary>
+    /// stackに上限をつけて、上限に到達したら古いものから半分削除する
+    /// </summary>
+    public void CheckStackWeight()
+    {
+        int max = 40;
+        int deleted = (int)(max * 0.5);
+
+        int UndoStackCount = UndoStack.Count;
+
+        if (UndoStackCount < max)
+            return;
+
+        Stack<Command> WorkStack = new Stack<Command>();
+        for (int i = 0; i < UndoStackCount; i++)
+        {
+            WorkStack.Push(UndoStack.Pop());
+        }
+
+        for (int i = 0; i < UndoStackCount; i++)
+        {
+            if(i < deleted)
+            {
+                WorkStack.Pop();
+            }
+            else
+            {
+                UndoStack.Push(WorkStack.Pop());
+            }
+        }
+
+        // TODO GCを解放する
     }
 }
